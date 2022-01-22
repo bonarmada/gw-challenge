a4d712b78e01f501995c497b5bbc5171add6a314
vaughn <bonarmada@gmail.com>
2022-01-22 18:46:34 +0800

    [room] Set up room db

--- app/src/main/java/io/github/bonarmada/gw_challenge/features/main/MainActivity.kt
+++ app/src/main/java/io/github/bonarmada/gw_challenge/features/main/MainActivity.kt
@@ -3,13 +3,12 @@ package io.github.bonarmada.gw_challenge.features.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import io.reactivex.subjects.BehaviorSubject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        
     }
 }
