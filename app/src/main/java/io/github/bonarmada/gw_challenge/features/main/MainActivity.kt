1636bb38a3ebc3b6ee912976bc3ec7ed8b993965
vaughn <bonarmada@gmail.com>
2022-01-22 18:33:24 +0800

    [rxjava] Set up rxjava3, rxbindings for views

--- app/src/main/java/io/github/bonarmada/gw_challenge/features/main/MainActivity.kt
+++ app/src/main/java/io/github/bonarmada/gw_challenge/features/main/MainActivity.kt
@@ -3,9 +3,10 @@ package io.github.bonarmada.gw_challenge.features.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.subjects.BehaviorSubject
 
 @AndroidEntryPoint
-class ExampleActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
